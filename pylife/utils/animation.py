d9409284f357418ba2215998ff405285a2c5baab
Marko <markogajiin@gmail.com>
2020-11-05 22:34:49 +0100

    animation function moved to utils

---
+++ pylife/utils/animation.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+def animate(steps):
+
+    def init():
+        im.set_data(steps[0])
+        return [im]
+
+    def animate(i):
+        im.set_data(steps[i])
+        return [im]
+
+    im = plt.matshow(steps[0], interpolation='None', animated=True)
+    figure = im.get_figure()
+
+    return FuncAnimation(figure, animate, init_func=init, frames=len(steps), interval=500, blit=True, repeat=False)
